b7696b7f111f81cfb71691216c171d780b8ada79
Noor Farhana Binti Zainal Abidin <farhanazainal.a99@gmail.com>
2023-09-05 23:49:53 +0800

    Uploading Mae-Dev

---
+++ order.js
@@ -0,0 +1,29 @@
+const Orders = [
+    {
+        productName: 'HTML Beginner',
+        productNumber: 'WEB101',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+
+    {
+        productName: 'CSS Beginner',
+        productNumber: 'STYL102',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+
+    {
+        productName: 'JavaScript Full Course',
+        productNumber: 'DEV200',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'PHP Beginner',
+        productNumber: 'PHP101',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+];
---
+++ test.js
@@ -0,0 +1,8 @@
+let planetName = prompt("What is the name of the new planet?", "Enter Planet Name");
+
+alert(`The new planet is ${planetName}`)
+
+
+let isBoss = confirm("Are you the boss?");
+
+alert(isBoss);
